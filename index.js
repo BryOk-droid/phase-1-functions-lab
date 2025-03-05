ea28161a4844d5ea9ab1e003fda8eaf9e0466be3
BryOk-droid <brianokoth534@gmail.com>
2025-03-05 12:08:19 +0300

    Implemented functions for distance calculations and fare pricing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42); // HQ is on 42nd Street
+}
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264; // Each block is 264 feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0; 
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; 
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; 
+  } else {
+    return "cannot travel that far"; 
+  }
+}
